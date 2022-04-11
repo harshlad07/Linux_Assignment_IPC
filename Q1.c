b9392a92990088db3f10c3f040921e75798e916f
harshlad07 <74580401+harshlad07@users.noreply.github.com>
2022-04-11 11:16:32 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -5,17 +5,20 @@
 
 int main()
 {
-char *pnt;
-int ID;
-id=shmget(48,250,IPC_CREAT|0644);
-if(ID<0)
-{
-perror("shmget");
-return 0;
-}
-printf("ID is = %d\n",ID);
-p=shmat(ID,0,0);
-printf("Data:");
-scanf("%s",pnt);
-return 0;
+  char *pnt;
+  int ID;
+  id=shmget(48,250,IPC_CREAT|0644);
+  
+  if(ID<0)
+  {
+    perror("shmget");
+    return 0;
+  }
+  
+  printf("ID is = %d\n",ID);
+  p=shmat(ID,0,0);
+  printf("Data is - ");
+  scanf("%s\n",pnt);
+  
+  return 0;
 }
