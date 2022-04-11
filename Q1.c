7f6778e35e3ce4a014da607d2b3bebd3d5c9d29b
harshlad07 <74580401+harshlad07@users.noreply.github.com>
2022-04-11 11:19:45 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+
+int main()
+{
+  char *pnt;
+  int ID;
+  id=shmget(48,250,IPC_CREAT|0644);
+  
+  if(ID<0)
+  {
+    perror("shmget");
+    return 0;
+  }
+  
+  printf("ID is = %d\n",ID);
+  p=shmat(ID,0,0);
+  printf("Data is - ");
+  scanf("%s\n",pnt);
+  
+  return 0;
+}
